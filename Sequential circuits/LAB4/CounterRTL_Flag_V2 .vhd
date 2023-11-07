dbb3e9c099cd8b8f4b1a0acaaa08cbdb9750e9ef
IHEEB22 <chihebeddine.hmida@ensi-uma.tn>
2023-11-07 19:30:32 +0100

    Update CounterRTL_Flag_V2 .vhd

--- Sequential circuits/LAB4/CounterRTL_Flag_V2 .vhd
+++ Sequential circuits/LAB4/CounterRTL_Flag_V2 .vhd
@@ -24,16 +24,16 @@ begin
     S <= S + 1;     
   end if  ;
   
-  -- Set Flag to 1 if we comple a cycle count (0-->7)
-  if S = "111" then
-    F_signal<= '1'; -- We can't use this instruction on F directrely cause F is declared an output  
-  else
-    F_signal<= '0';
-  end if;
+  -- Set Flag to 1 if we comple a cycle count (0-->7) [Remove this Block]
+  -- if S = "111" then
+  --  F_signal<= '1'; -- We can't use this instruction on F directrely cause F is declared an output
+  -- else
+  --  F_signal<= '0';
+  -- end if;
  
  end process ;
- 
- 
+
+-- Set Flag to 1 if we comple a cycle count (0-->7)
   process(S)
    begin
      if S="111" then
